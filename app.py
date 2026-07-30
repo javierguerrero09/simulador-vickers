0c25f1c0e1d0075fe46973f6b1882bc0a171d7d1
Javier Paolantonio Guerrero <javierguerrero09@gmail.com>
2026-07-30 16:40:33 -0300

    Update app.py
    no comments

--- app.py
+++ app.py
@@ -2,26 +2,21 @@ import os
 import requests
 import streamlit as st
 
-# 1. Traer de forma oculta el token desde las variables del servidor
 GITHUB_TOKEN = os.environ.get("MY_GITHUB_TOKEN")
 
-# 2. Configurar la ruta de tu archivo privado
-# Reemplazá 'TU_USUARIO' y 'TU_REPO_PRIVADO' con tus datos reales de GitHub
 USER = "javierguerrero09"
 REPO = "simulador-vickers-privado"
 FILE_PATH = "vickers_sim.py"
-BRANCH = "main" # o 'master', según cómo se llame tu rama principal
+BRANCH = "main"
 
 url = f"https://raw.githubusercontent.com/{USER}/{REPO}/{BRANCH}/{FILE_PATH}"
 
-# 3. Autenticarse contra GitHub de manera invisible para el cliente
 headers = {"Authorization": f"token {GITHUB_TOKEN}"}
 
 try:
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
-        # Se descarga el código en la memoria RAM del servidor y se ejecuta
         codigo_fuente = response.text
         exec(codigo_fuente)
     else:
